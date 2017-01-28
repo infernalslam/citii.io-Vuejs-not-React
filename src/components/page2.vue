43433258faa69b7675454510a17b95b20df21d91
infernalslam <infernal-slam@hotmail.com>
2017-01-28 15:33:44 +0700

    :muscle:

--- src/components/page2.vue
+++ src/components/page2.vue
@@ -1,21 +1,66 @@
 <template lang="html">
   <div class="aligner">
-    <router-link to="/"><div class="menu"><button class="continue-button">Menu</button></div></router-view>
+    <router-link to="/"><div class="menu"><button class="continue-button">Menu</button></div></router-link>
 
     <div class="container">
       <div class="row">
 
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="margin-top: 35vh;">
           <p class="font-title">What city do you currently live in?</p>
-        </div>
+          <basic-select :options="options":selected-option="item" @select="onSelect"></basic-select>
 
+          <p class="text-city">{{item.text}}</p>
+
+          <div :style="{ display: dis }">
+            <i class="fa fa-arrow-right arrow-button" aria-hidden="true"></i>
+          </div>
+
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { BasicSelect } from 'vue-search-select'
+import store from '../vuex/store.js'
 export default {
+  name: 'page2',
+  data () {
+    return {
+      store,
+      options: store.state.costOflive,
+      searchText: '',
+      item: {
+        value: '',
+        text: '',
+        slug: '',
+        currency_type: '',
+        index: '',
+        rent_index: '',
+        groceries_index: '',
+        restaurant_index: '',
+        purchasing_index: ''
+      },
+      dis: 'none'
+    }
+  },
+  methods: {
+    onSelect (item) {
+      this.item = item
+      if (this.item.value && this.item.text) {
+        this.dis = 'inline-block'
+        console.log('มีค่า')
+      } else {
+        this.dis = 'none'
+        console.log('ไม่มีค่า')
+      }
+      console.log('on select ', this.item)
+    }
+  },
+  components: {
+    BasicSelect
+  }
 }
 </script>
 
@@ -54,6 +99,35 @@ export default {
     background-color: #ea4c88;
     border-color: #ea4c88;
     margin: 0 10px;
+    -o-transition: .5s;
+    -ms-transition: .5s;
+    -moz-transition: .5s;
+    -webkit-transition: .5s;
+    transition: .5s;
+}
+
+.text-city {
+  color: rgb(255, 255, 255);
+  font-family: Nunito, sans-serif;
+  font-size: 30px;
+  text-align: center;
+  margin-top: 32px;
+}
+
+.next-but {
+  display: inline-block;
+}
+
+.arrow-button:hover {
+    background-color: #E83479;
+}
+.arrow-button {
+    color: #FFF;
+    border-radius: 60px;
+    padding: 10px;
+    cursor: pointer;
+    background: #EA4C88;
+    margin: 10px;
     -o-transition: .5s;
     -ms-transition: .5s;
     -moz-transition: .5s;
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,10 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 Vue.use(Vuex)
+var dataCovertdataCostOfLive = []
 var dataCostOfLive = require('./cost_of_living_indices.json')
+for (var key in dataCostOfLive) {
+  if (dataCostOfLive.hasOwnProperty(key)) {
+    let setData = {
+      value: new Date(),
+      text: key,
+      slug: dataCostOfLive[key].slug,
+      currency_type: dataCostOfLive[key].currency_type,
+      index: dataCostOfLive[key].index,
+      rent_index: dataCostOfLive[key].rent_index,
+      groceries_index: dataCostOfLive[key].groceries_index,
+      restaurant_index: dataCostOfLive[key].restaurant_index,
+      purchasing_index: dataCostOfLive[key].purchasing_index
+    }
+    dataCovertdataCostOfLive.push(setData)
+  }
+}
 
 const state = {
-  costOflive: dataCostOfLive
+  costOflive: dataCovertdataCostOfLive
 }
 /* mutations */
 const mutations = {
